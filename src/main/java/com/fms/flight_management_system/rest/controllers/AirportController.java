49d82880c2203cf81dd21f6af29007561b4a450b
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-24 18:55:28 +0530

    DA-56 Basic Structure of Airport

---
+++ src/main/java/com/fms/flight_management_system/rest/controllers/AirportController.java
@@ -0,0 +1,9 @@
+package com.fms.flight_management_system.rest.controllers;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 24/05/22
+ */
+public interface AirportController {
+}
