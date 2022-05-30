0f1c6b95301af5bb2c4878b8e8f7eb943193e690
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-30 18:54:29 +0530

    DA-57 Get All Airports

---
+++ src/main/java/com/fms/flight_management_system/rest/dtos/AirportRequestDto.java
@@ -0,0 +1,9 @@
+package com.fms.flight_management_system.rest.dtos;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 25/05/22
+ */
+public class AirportRequestDto {
+}
--- src/main/java/com/fms/flight_management_system/service/impl/AirportServiceImpl.java
+++ src/main/java/com/fms/flight_management_system/service/impl/AirportServiceImpl.java
@@ -1,9 +1,40 @@
 package com.fms.flight_management_system.service.impl;
 
+import com.fms.flight_management_system.model.Airport;
+import com.fms.flight_management_system.repository.AirportRepository;
+import com.fms.flight_management_system.service.AirportService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author <a>Pulkit Aggarwal</a>
  * @version 1.0
  * @since 24/05/22
  */
-public class AirportServiceImpl {
+@AllArgsConstructor
+@Slf4j
+@Service
+public class AirportServiceImpl implements AirportService {
+
+	private final AirportRepository airportRepository;
+
+	@Override public List<Airport> getAllAirports() {
+		return airportRepository.findAll();
+	}
+
+	@Override public Optional<Airport> getAirportByAirportCode(String airportCode) {
+		return Optional.empty();
+	}
+
+	@Override public void saveAirport(Airport airport) {
+
+	}
+
+	@Override public void deleteAirport(String airportCode) {
+
+	}
 }
