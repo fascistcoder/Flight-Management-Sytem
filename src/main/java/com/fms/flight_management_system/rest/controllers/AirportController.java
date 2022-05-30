63846a234a781ae8be9ec1e92d9223ca6acff02f
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-05-30 19:08:49 +0530

    DA-59 Get Airport

--- src/main/java/com/fms/flight_management_system/rest/controllers/AirportController.java
+++ src/main/java/com/fms/flight_management_system/rest/controllers/AirportController.java
@@ -1,9 +1,19 @@
 package com.fms.flight_management_system.rest.controllers;
 
+import com.fms.flight_management_system.model.Airport;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 /**
  * @author <a>Pulkit Aggarwal</a>
  * @version 1.0
  * @since 24/05/22
  */
 public interface AirportController {
+
+	@GetMapping("/v1/show-airports-list") ResponseEntity<List<Airport>> showAllAirports();
+
+
 }
--- src/main/java/com/fms/flight_management_system/rest/controllers/impl/AirportControllerImpl.java
+++ src/main/java/com/fms/flight_management_system/rest/controllers/impl/AirportControllerImpl.java
@@ -1,9 +1,22 @@
 package com.fms.flight_management_system.rest.controllers.impl;
 
+import com.fms.flight_management_system.model.Airport;
+import com.fms.flight_management_system.rest.controllers.AirportController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author <a>Pulkit Aggarwal</a>
  * @version 1.0
  * @since 24/05/22
  */
-public class AirportControllerImpl {
+@RestController
+@AllArgsConstructor
+public class AirportControllerImpl implements AirportController {
+	@Override public ResponseEntity<List<Airport>> showAllAirports() {
+		return null;
+	}
 }
