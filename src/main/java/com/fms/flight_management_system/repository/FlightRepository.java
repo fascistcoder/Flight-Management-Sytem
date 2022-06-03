c221f0bbee94a764df845e05a6a7410d994cff13
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-06-03 22:07:05 +0530

    DA-101 Flight Structure

---
+++ src/main/java/com/fms/flight_management_system/repository/FlightRepository.java
@@ -0,0 +1,14 @@
+package com.fms.flight_management_system.repository;
+
+import com.fms.flight_management_system.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 03/06/22
+ */
+public interface FlightRepository extends JpaRepository<Flight, UUID> {
+}
---
+++ src/main/java/com/fms/flight_management_system/rest/controllers/impl/FlightControllerImpl.java
@@ -0,0 +1,9 @@
+package com.fms.flight_management_system.rest.controllers.impl;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 03/06/22
+ */
+public class FlightControllerImpl {
+}
