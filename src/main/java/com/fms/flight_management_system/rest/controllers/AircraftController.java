5264c647ad456c05691cf004e0a759c56f19886a
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-06-03 21:38:41 +0530

    DA-100 Aircraft Basic Structure

---
+++ src/main/java/com/fms/flight_management_system/rest/controllers/AircraftController.java
@@ -0,0 +1,9 @@
+package com.fms.flight_management_system.rest.controllers;
+
+/**
+ * @author <a>Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 03/06/22
+ */
+public interface AircraftController {
+}
